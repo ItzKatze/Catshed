7c599924a13c1d831cecdb35a6a7f1be1ea969bd
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2023-06-10 18:42:50 +0200

    Delete Catshed directory

--- Catshed/index.js
+++
@@ -1,6 +0,0 @@
-import request from "../requestV2";
-import Settings from "./settings";
-import slayerDrops from "./drops/slayerDrops";
-import farmingDrops from "./drops/farmingDrops";
-
-register("command", () => Settings.openGUI()).setName("catshed");
