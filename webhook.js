02e88fcf9ab28487fd374f410e5617c37f49b829
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-06-23 13:00:42 +0200

    0.0.8

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@ class webhookdata {
 
     username = "Catshed";
     avatar_url = "https://i.imgur.com/WYh0p9a.png";
-    footer = "Catshed v0.0.7";
+    footer = "Catshed v0.0.8";
 };
 
 export default new webhookdata();
